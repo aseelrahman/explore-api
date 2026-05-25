0c22b36af5321756e6f9a7c108b0445987bda638
Aseel Rahman <132919956+aseelrahman@users.noreply.github.com>
2026-05-25 09:50:34 -0400

    load data

--- script.js
+++ script.js
@@ -3,3 +3,25 @@ fetch('https://jsonplaceholder.typicode.com/todos/1')
 .then((data) => console.log(data))
 
 // console.log(result);
+
+const loadData = () => {
+    // promise of response
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+    // promise of json data
+    .then(response => response.json())
+    .then((data) => console.log(data))
+} 
+
+const loadPost = () => {
+    const url = 'https://jsonplaceholder.typicode.com/posts#'
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayPost(data))
+}
+
+const displayPost = (posts) => {
+    posts.forEach(post => {
+        console.log(post);
+    });
+    
+}
