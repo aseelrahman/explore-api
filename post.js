a74dda3f18bb9c91ec23728f03c041c97661febd
Aseel Rahman <132919956+aseelrahman@users.noreply.github.com>
2026-05-25 11:57:29 -0400

    show posts automatically

--- post.js
+++ post.js
@@ -8,19 +8,32 @@ const loadPost = () =>{
     })
 }
 
-const displayPost = (posts) => {
+// {
+//     "userId": 10,
+//     "id": 95,
+//     "title": "id minus libero illum nam ad officiis",
+//     "body": "earum voluptatem facere provident blanditiis velit laboriosam\npariatur accusamus odio saepe\ncumque dolor qui a dicta ab doloribus consequatur omnis\ncorporis cupiditate eaque assumenda ad nesciunt"
+// }
 
-    // 1. get the container
-    const postContainer = document.getElementById('post-container');
-    // console.log(postContainer);
+const displayPost = (posts) => {
+    // 1. get the container and empty the container
+    const postsContainer = document.getElementById('post-container');
+    postsContainer.innerHTML = '';
     
     posts.forEach(post => {
-        // console.log(post.title);
-        // 2. create HTML element
-        const li = document.createElement('li');
-        li.innerText = post.title;
-
-        // 3. add li into container
-        postContainer.appendChild(li)
+        // 2. create element
+        
+        const postCard = document.createElement('div');
+        postCard.innerHTML = `
+        <div class="post-card">
+            <h2>${post.title}</h2>
+            <p>${post.body}</p>
+        </div>
+        `
+        // 3. add to container
+        postsContainer.appendChild(postCard)
+        
     });
 }
+
+loadPost();
